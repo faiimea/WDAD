e9dd22de24e105d703e056b0ba6ccd9e1c6f6b95
Yanxing <1320407956@qq.com>
2023-03-03 18:35:35 +0800

    demo

---
+++ PRP_demo/feature/gray_test.py
@@ -0,0 +1,234 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import torch
+import pandas as pd
+# np.set_printoptions(threshold=np.sys.maxsize)
+from PIL import Image
+import scipy.io as io
+
+
+def fast_glcm(img, vmin=0, vmax=255, nbit=8, kernel_size=5):
+    mi, ma = vmin, vmax
+    ks = kernel_size
+    h,w = img.shape
+    # digitize
+    bins = np.linspace(mi, ma+1, nbit+1)#将原来的255级别调整到8级别
+    gl1 = np.digitize(img, bins) - 1#img原有数值在bins中的位置，比如0-32就是1,32-64就是2。
+    gl2 = np.append(gl1[:,1:], gl1[:,-1:], axis=1)
+    #需要被添加的数组除去第一列的gl1,添加的数组是gl1的除去最后一列，添加是沿着列添加的。
+    # make glcm
+    glcm = np.zeros((nbit, nbit, h, w), dtype=np.uint8)
+    for i in range(nbit):
+        for j in range(nbit):
+            mask = ((gl1==i) & (gl2==j))
+            glcm[i,j, mask] = 1
+    kernel = np.ones((ks, ks), dtype=np.uint8)
+    for i in range(nbit):
+        for j in range(nbit):
+            glcm[i,j] = cv2.filter2D(glcm[i,j], -1, kernel)
+    glcm = glcm.astype(np.float32)
+    return glcm
+
+def fast_glcm_mean(img, vmin=0, vmax=255, nbit=8, ks=5):
+    '''
+    calc glcm mean
+    '''
+    h,w = img.shape
+    glcm = fast_glcm(img, vmin, vmax, nbit, ks)
+    mean = np.zeros((h,w), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            mean += glcm[i,j] * i / (nbit)**2
+    return mean
+
+def fast_glcm_std(img, vmin=0, vmax=255, nbit=8, ks=5):
+    '''
+    calc glcm std
+    '''
+    h,w = img.shape
+    glcm = fast_glcm(img, vmin, vmax, nbit, ks)
+    mean = np.zeros((h,w), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            mean += glcm[i,j] * i / (nbit)**2
+    std2 = np.zeros((h,w), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            std2 += (glcm[i,j] * i - mean)**2
+    std = np.sqrt(std2)
+    return std
+
+def fast_glcm_contrast(img, vmin=0, vmax=255, nbit=8, ks=5):
+    '''
+    calc glcm contrast
+    '''
+    h,w = img.shape
+    glcm = fast_glcm(img, vmin, vmax, nbit, ks)
+    cont = np.zeros((h,w), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            cont += glcm[i,j] * (i-j)**2
+    return cont
+
+def fast_glcm_dissimilarity(img, vmin=0, vmax=255, nbit=8, ks=5):
+    '''
+    calc glcm dissimilarity
+    '''
+    h,w = img.shape
+    glcm = fast_glcm(img, vmin, vmax, nbit, ks)
+    diss = np.zeros((h,w), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            diss += glcm[i,j] * np.abs(i-j)
+    return diss
+
+def fast_glcm_homogeneity(img, vmin=0, vmax=255, nbit=8, ks=5):
+    '''
+    calc glcm homogeneity
+    '''
+    h,w = img.shape
+    glcm = fast_glcm(img, vmin, vmax, nbit, ks)
+    homo = np.zeros((h,w), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            homo += glcm[i,j] / (1.+(i-j)**2)
+    return homo
+
+def fast_glcm_ASM(img, vmin=0, vmax=255, nbit=8, ks=5):
+    '''
+    calc glcm asm, energy
+    '''
+    h,w = img.shape
+    glcm = fast_glcm(img, vmin, vmax, nbit, ks)
+    asm = np.zeros((h,w), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            asm  += glcm[i,j]**2
+    ene = np.sqrt(asm)
+    return asm, ene
+
+def fast_glcm_max(img, vmin=0, vmax=255, nbit=8, ks=5):
+    '''
+    calc glcm max
+    '''
+    glcm = fast_glcm(img, vmin, vmax, nbit, ks)
+    max_  = np.max(glcm, axis=(0,1))
+    return max_
+
+def fast_glcm_entropy(img, vmin=0, vmax=255, nbit=8, ks=5):
+    '''
+    calc glcm entropy
+    '''
+    glcm = fast_glcm(img, vmin, vmax, nbit, ks)
+    pnorm = glcm / np.sum(glcm, axis=(0,1)) + 1./ks**2
+    ent  = np.sum(-pnorm * np.log(pnorm), axis=(0,1))
+    return ent
+
+#应用于本地图片
+image = r'PRP/dl_learn/output1_1.png'
+image2=r'PRP/dl_learn/output1_2.png'
+# image = r'adversarial-attacks-pytorch-3.3.0/demo/ORG_img/_0.png'
+# image2=r'adversarial-attacks-pytorch-3.3.0/demo/FGSM_img/_0.png'
+img=np.array(Image.open(image).convert('L'))
+img2=np.array(Image.open(image2).convert('L'))
+#img = data.camera()
+# plt.imshow(img,cmap=plt.cm.gray)
+h,w = img.shape
+#计算各个统计值
+mean = fast_glcm_mean(img)
+std = fast_glcm_std(img)
+cont = fast_glcm_contrast(img)
+diss = fast_glcm_dissimilarity(img)
+homo = fast_glcm_homogeneity(img)
+asm, ene = fast_glcm_ASM(img)
+ma = fast_glcm_max(img)
+ent = fast_glcm_entropy(img)
+
+mean2 = fast_glcm_mean(img2)
+std2 = fast_glcm_std(img2)
+cont2 = fast_glcm_contrast(img2)
+diss2 = fast_glcm_dissimilarity(img2)
+homo2 = fast_glcm_homogeneity(img2)
+asm2, ene2 = fast_glcm_ASM(img2)
+ma2 = fast_glcm_max(img2)
+ent2 = fast_glcm_entropy(img2)
+#绘制各个特征的子图
+plt.figure(figsize=(10,4.5))
+fs = 15
+plt.subplot(2,5,1)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.imshow(img,cmap=plt.cm.gray)
+plt.title('original', fontsize=fs)
+plt.subplot(2,5,2)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.imshow(mean)
+plt.title('mean', fontsize=fs)
+plt.subplot(2,5,3)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.imshow(std)
+plt.title('std', fontsize=fs)
+plt.subplot(2,5,4)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.imshow(cont)
+plt.title('contrast', fontsize=fs)
+
+plt.subplot(2,5,5)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.imshow(diss)
+plt.title('dissimilarity', fontsize=fs)
+plt.subplot(2,5,6)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.imshow(homo)
+plt.title('homogeneity', fontsize=fs)
+plt.subplot(2,5,7)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.imshow(asm)
+plt.title('ASM', fontsize=fs)
+plt.subplot(2,5,8)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.imshow(ene)
+plt.title('energy', fontsize=fs)
+plt.subplot(2,5,9)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.imshow(ma)
+plt.title('max', fontsize=fs)
+plt.subplot(2,5,10)
+plt.tick_params(labelbottom=False, labelleft=False)
+plt.imshow(ent)
+plt.title('entropy', fontsize=fs)
+plt.tight_layout(pad=0.5)
+# print(asm)
+# result1 = np.array(asm)
+# result1=np.round(
+#     result1,
+#     decimals=1
+# )
+#
+# np.savetxt('npresult1.txt',result1)
+# io.savemat('save.mat',{'result1':result1})
+
+# print(asm)
+# plt.show()
+#1-183行可以得到灰度共生矩阵，不同类型处理后的特征图像
+
+smean=np.sum((mean-mean2)*(mean-mean2))/100.0
+sstd=np.sum((std-std2)*(std-std2))/100.0
+scont=np.sum((cont-cont2)*(cont-cont2))/100.0
+sdiss=np.sum((diss-diss2)*(diss-diss2))/100.0
+shomo=np.sum((homo-homo2)*(homo-homo2))/100.0
+sasm=np.sum((asm-asm2)*(asm-asm2))/100.0
+sma=np.sum((ma-ma2)*(ma-ma2))/100.0
+sent=np.sum((ent-ent2)*(ent-ent2))/100.0
+sene=np.sum((ene-ene2)*(ene-ene2))/100.0
+print('比对各个灰度图像下的差别')
+print(1)
+print('mean:%d'%(smean))
+print('std:%d'%sstd)
+print('cont:%d'%scont)
+print('diss:%d'%sdiss)
+print('homo:%d'%shomo)
+print('asm:%d'%sasm)
+print('ma:%d'%sma)
+print('ent:%d'%sent)
+print('ene:%d'%sene)
---
+++ PRP_demo/feature/process.py
@@ -0,0 +1,98 @@
+import os
+import cv2
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+
+# 对图像进行灰度处理，得到灰度共生矩阵
+def fast_glcm(img, vmin=0, vmax=255, nbit=8, kernel_size=5):
+    mi, ma = vmin, vmax
+    ks = kernel_size
+    h,w = img.shape
+    # digitize
+    bins = np.linspace(mi, ma+1, nbit+1)    # 将原来的255级别调整到8级别
+    gl1 = np.digitize(img, bins) - 1    # img原有数值在bins中的位置，比如0-32就是1,32-64就是2。
+    gl2 = np.append(gl1[:,1:], gl1[:,-1:], axis=1)
+    # 需要被添加的数组除去第一列的gl1,添加的数组是gl1的除去最后一列，添加是沿着列添加的。
+    # make glcm
+    glcm = np.zeros((nbit, nbit, h, w), dtype=np.uint8)
+    for i in range(nbit):
+        for j in range(nbit):
+            mask = ((gl1==i) & (gl2==j))
+            glcm[i,j, mask] = 1
+    kernel = np.ones((ks, ks), dtype=np.uint8)
+    for i in range(nbit):
+        for j in range(nbit):
+            glcm[i,j] = cv2.filter2D(glcm[i,j], -1, kernel)
+    glcm = glcm.astype(np.float32)
+    return glcm
+
+# 利用对抗前后，灰度特征变化最明显的asm参数，计算其矩阵
+def fast_glcm_ASM(img, vmin=0, vmax=255, nbit=8, ks=5):
+    '''
+    calc glcm asm, energy
+    '''
+    h,w = img.shape
+    glcm = fast_glcm(img, vmin, vmax, nbit, ks)
+    asm = np.zeros((h,w), dtype=np.float32)
+    for i in range(nbit):
+        for j in range(nbit):
+            asm  += glcm[i,j]**2
+    ene = np.sqrt(asm)
+    # asm=Image.fromarray(asm)
+    # trans_resize=transforms.Resize([256,256])
+    # asm=trans_resize(asm)
+    return asm, ene
+
+# def ToBGR(save_path,img_path="D:/PRP_lfz/PRP/ORG_img"):
+#     #列出指定路径下的所有文件
+#     imgPdir=os.listdir(img_path)
+#     imgdir=img_path+"/"+imgPdir[1]
+#     # print(imgPdir)
+#     s=imgdir
+#     img=np.array(Image.open(s).convert('L'))
+#     h,w = img.shape
+#     asm,ene=fast_glcm_ASM(img)
+#     #asm即为所需
+#     img_file="D:/PRP_lfz/PRP/GRAY_img"
+#     if not os.path.exists(img_file):
+# 	    os.mkdir(img_file)
+#     cv2.imwrite(img_file,asm)
+    # plt.imshow(tr)
+    # plt.show()
+    # for imgdir in imgPdir:
+    # #列出当前子文件下的图片(注意这里的路径需要完整的路径，不然找不到图片的路径)
+    #     imgdir=img_path+"/"+imgdir
+    #     tr=cv2.read(imgdir)
+        # img=os.listdir(imgdir)
+        #将当前列出的图片进行转换
+#     for cimg in imgPdir:
+#         # 打开文件
+#         openimg=imgPdir+"/"+cimg
+#         newImg=cv2.imread(openimg)
+#         #以PNG格式保存到指定的路径
+#         saveImg=os.path.join(save_path,imgfile)
+#             #saveImg=data/train/001.png...
+#         cv2.imwrite(saveImg,newImg)
+#     print('正在转换...')
+# print("完成转换!\n")
+ 
+ 
+# if __name__ == '__main__':
+    # ToBGR(save_path="data/val")
+img_path="D:/PRP_lfz/PRP/FGSM_img"
+imgPdir=os.listdir(img_path)
+for i in range(len(imgPdir)):
+    imgdir=img_path+"/"+imgPdir[i]
+    # print(imgPdir)
+    img=np.array(Image.open(imgdir).convert('L'))
+    h,w = img.shape
+    asm,ene=fast_glcm_ASM(img)
+    #asm即为所需
+    # plt.imshow(asm)
+    # print(asm.shape)
+    save_path="D:/PRP_lfz/PRP/GRAY_FGSM_img/"+str(i)+".jpg"
+    cv2.imwrite(save_path,asm)
+# # Let
+# # plt.show()
